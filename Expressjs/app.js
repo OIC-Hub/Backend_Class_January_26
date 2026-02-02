6c0eef585c0fe3ee1eaa46ea41390ca9eeeb25c0
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-02-02 11:25:33 +0100

    2/2/2026

--- Expressjs/app.js
+++ Expressjs/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const contactRoute = require("./Routes/contact");
 const bodyParser = require("body-parser");
-
+const connectDB = require("./config/db")
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT ;
@@ -13,6 +13,8 @@ app.use(bodyParser.json());
 
 
 app.use("/api", contactRoute);
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
