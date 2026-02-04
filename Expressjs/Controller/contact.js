d703df67d51a7ebe53ef284a8e07bd11bfde0fc1
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-02-04 11:07:00 +0100

    update

--- Expressjs/Controller/contact.js
+++ Expressjs/Controller/contact.js
@@ -1,19 +1,17 @@
 const contact = require("../Model/data");
-const Product = require("../Model/product")
+const Product = require("../Model/product");
 
 function AddData(req, res) {
-  
-    const { name, phoneNo } = req.body;
-  
-    const newData = {
-      id: contact.length + 1,
-      name: name,
-      phoneNo: phoneNo,
-    };
+  const { name, phoneNo } = req.body;
 
-    contact.push(newData);
-    res.status(200).json({message: "Contact Added", contact})
+  const newData = {
+    id: contact.length + 1,
+    name: name,
+    phoneNo: phoneNo,
+  };
 
+  contact.push(newData);
+  res.status(200).json({ message: "Contact Added", contact });
 }
 
 function getData(req, res) {
@@ -25,21 +23,41 @@ function getData(req, res) {
 }
 
 function AddProduct(req, res) {
-try {
-    const{productName, productImage, productPrice} = req.body;
-  
-  const newProduct = new Product ({
-    productName,
-    productImage,
-    productPrice,
-  })
+  try {
+    const { productName, productImage, productPrice } = req.body;
 
-  newProduct.save();
-  res.status(200).json({message: "product added successfully"});
-} catch (error) {
-  console.error(error)
-}
-  
+    const newProduct = new Product({
+      productName,
+      productImage,
+      productPrice,
+    });
+
+    newProduct.save();
+    res.status(200).json({ message: "product added successfully" });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-module.exports = { AddData, getData, AddProduct };
+const getProduct = async (req, res) => {
+  try {
+    const allProduct = await Product.find();
+    res.status(200).json({ message: "Products", allProduct });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { productName, productImage, productPrice } = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(id, { productName, productImage, productPrice }, { new: true } );
+    
+    res.status(200).json({ message: "updated", updatedProduct });
+  } catch (error) {
+    console.error(error)
+  }
+};
+module.exports = { AddData, getData, AddProduct, getProduct, updateProduct};
--- Expressjs/Routes/contact.js
+++ Expressjs/Routes/contact.js
@@ -1,10 +1,12 @@
 const express = require("express")
-const {AddData, getData, AddProduct} = require("../Controller/contact")
+const {AddData, getData, AddProduct, getProduct, updateProduct} = require("../Controller/contact")
 const router = express.Router();
 
 router.post("/add", AddData);
 router.get("/get", getData);
 router.post("/product", AddProduct)
+router.get("/products", getProduct)
+router.put("/product/:id", updateProduct)
 
 module.exports = router;
 
